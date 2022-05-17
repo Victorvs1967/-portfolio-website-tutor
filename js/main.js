7d8ee2179eee8fbeb3005a293400cfacdf77d808
Victor <victorsmirnov@me.com>
2022-05-17 21:08:05 +0300

    create portfolio website

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const pageTransition = () => {
+  // button click to active class
+  [ ...document.querySelectorAll('.control') ].forEach(control => {
+    control.addEventListener('click', () => {
+      // remove active-btn class from all controlls
+      document.querySelectorAll('.active-btn').forEach(ctrl => {
+        ctrl.classList.remove('active-btn');
+      });
+      // add active class to selected
+      control.classList.add('active-btn');
+      document.querySelector('.active').classList.remove('active');
+      document.getElementById(control.dataset.id).classList.add('active');
+    });
+  });
+};
+// call main function
+pageTransition();
